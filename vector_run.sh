d5abeccc0450f7ccbf9852c08c356d57b1060ea4
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-10 02:16:57 -0400

    add sleep

--- vector_run.sh
+++ vector_run.sh
@@ -8,6 +8,11 @@
 #SBATCH --output=logs/%x-%A-%a.out
 #SBATCH --error=logs/%x-%A-%a.err
 
+# Introduce a delay based on the task ID
+sleep_time=$((SLURM_ARRAY_TASK_ID * 10))  # 10 seconds delay for each task
+echo "Sleeping for ${sleep_time} seconds before starting job ${SLURM_ARRAY_TASK_ID}"
+sleep ${sleep_time}
+
 config=config_combo.txt
 
 name=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
