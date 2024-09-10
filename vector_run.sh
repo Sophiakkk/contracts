6f57c58470a682b2319097bd7c9c3ae0826b288f
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-09 23:43:10 -0400

    generate sh file

--- vector_run.sh
+++ vector_run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #SBATCH --qos=deadline
 #SBATCH --mem=4G
-#SBATCH --time=0-24:00:00
-#SBATCH --ntasks=1
+#SBATCH --time=0-5:00:00
+#SBATCH --ntasks=4
 #SBATCH --partition=cpu
 #SBATCH --cpus-per-task=1
 #SBATCH --array=1-20
 #SBTACH --output=logs/array_%A_%a.out
 
+config=config_combo.txt
 
+name=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $config)
 
+config_path=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $config)
 
-python runner.py --name "cleanup-complete" --config_path "experiment_configs/cleanup-contracting.json" 
+python runner.py --config_path $config_path --name $name
