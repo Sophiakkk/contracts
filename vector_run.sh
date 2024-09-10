c53601cd929ec10ef0d16879cf1af2a517a53994
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-10 02:30:43 -0400

    update sh

--- vector_run.sh
+++ vector_run.sh
@@ -2,7 +2,8 @@
 #SBATCH --qos=deadline
 #SBATCH --mem=4G
 #SBATCH --time=0-5:00:00
-#SBATCH --partition=cpu
+#SBATCH --partition=rtx6000
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4
 #SBATCH --array=1-20
 #SBATCH --output=logs/%x-%A-%a.out
