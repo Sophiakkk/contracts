d6dfe7ae4ca8b2ece31c5b1edaa529d1f87ee917
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-09 23:39:21 -0400

    generate job arrays

--- generate_config_combo.py
+++ generate_config_combo.py
@@ -1,13 +1,19 @@
 # experiment name
 exp_dics = {
-    {'PD_seperate':{
+    'PD_seperate':{
     'name':'pd_separate',
     'config_path': "experiment_configs/pd-seperate.json"
         },
     'PD_contract':{
     'name':'pd_contract',
     'config_path': "experiment_configs/pd-contract.json"}
-    }
 }
 
-
+id=1
+with open("config_combo.txt", "w") as f:
+    f.write("ArrayTaskID"+" "+"name"+" "+"config_path")
+    for key in exp_dics:
+        for num in range(10):
+            exp = exp_dics[key]  # Get the dictionary for the current key
+            f.write("\n"+str(id)+" "+exp['name']+" "+exp['config_path'])
+            id+=1
