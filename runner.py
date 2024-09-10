274524be88e578cd3f27706b7a176145257bd987
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-10 05:10:16 -0400

    add taskid

--- runner.py
+++ runner.py
@@ -90,7 +90,10 @@ if __name__ =='__main__' :
     # Load the config dict list 
     with open(args.config_path, 'r') as f:
             config_dict_list = json.load(f)
-    config_dict_list['task_id'] = args.task_id
+    task_config = {
+        "task_id": args.task_id,
+    }
+    config_dict_list.update(task_config)
 
     # The first element of the list is the global parameters which all configs will have (this will not overwrite any parameters in the individual configs)
     # The second element of the list is the parameters which will be iterated over (for example hyperparameter tuning) 
