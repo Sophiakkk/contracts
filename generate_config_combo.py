f9859ad35a1f4790072cad8e04d1666ce87101e3
zsh <zhuzhuhsm@gmail.com>
2024-09-13 00:25:42 -0400

    run contracts only

--- generate_config_combo.py
+++ generate_config_combo.py
@@ -1,9 +1,9 @@
 # experiment name
 exp_dics = {
-    'PD_seperate':{
-    'name':'pd_separate',
-    'config_path': "experiment_configs/pd-seperate.json"
-        },
+    # 'PD_seperate':{
+    # 'name':'pd_separate',
+    # 'config_path': "experiment_configs/pd-seperate.json"
+    #     },
     'PD_contract':{
     'name':'pd_contract',
     'config_path': "experiment_configs/pd-contract.json"}
--- vector_run.sh
+++ vector_run.sh
@@ -9,7 +9,7 @@
 #SBATCH -c 6
 #SBATCH --mem=4G
 
-#SBATCH --array=1-20%1
+#SBATCH --array=1-10
 #SBATCH --output=logs/%x-%A-%a.out
 #SBATCH --error=logs/%x-%A-%a.err
 
