01e28d08c7f1974c1258d449dbb1db29f29dbc0d
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-18 14:28:44 -0400

    change cpu cores

--- vector_run.sh
+++ vector_run.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=4G
 #SBATCH --time=0-24:00:00
 #SBATCH --partition=cpu
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=4
 #SBATCH --array=1-20
 #SBATCH --output=logs/%x-%A-%a.out
 #SBATCH --error=logs/%x-%A-%a.err
