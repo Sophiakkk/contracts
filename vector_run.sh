f05af7590d25715a47d4455d6093c6cb9ab613c0
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-18 00:35:00 -0400

    udpate resource usage

--- vector_run.sh
+++ vector_run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --account=deadline
-#SBATCH --qos=deadline
+#SBATCH --qos=cpu_qos
 
 #SBATCH --job-name="pd-all"
-#SBATCH --partition=a40,t4v1,t4v2   # Choose appropriate GPU partition
+#SBATCH --partition=cpu   # Choose appropriate GPU partition
 #SBATCH --gres=gpu:1          # Must request at least 1 GPU under deadline QoS
-#SBATCH --time=100:00:00
+#SBATCH --time=0-24:00:00
 #SBATCH -c 6
 #SBATCH --mem=4G
 
