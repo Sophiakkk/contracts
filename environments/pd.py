8b7cb14b5de2f7843197f0758d5864c690a90d6d
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-20 22:47:53 -0400

    load horizon in env

--- environments/pd.py
+++ environments/pd.py
@@ -16,7 +16,7 @@ class PD(MultiAgentEnv):
     NUM_STATES = 1 
 
     def __init__(self, config):
-        self.horizon = config.horizon
+        self.horizon = config['horizon']
         print("max_steps",self.horizon)
         # self.payout_mat = np.array([[-1., 0.], [-3., -2.]]) # for PD
         self.payout_mat = np.array([[0., 2.], [-1., 0.]]) # for IPC
