372bf2088427fa392bcb1155ecd842530023f562
Sophiakkk <zhuzhuhsm@gmail.com>
2024-09-22 03:51:49 -0400

    add grid game contract class

--- contract/contract_list.py
+++ contract/contract_list.py
@@ -3,6 +3,23 @@ from contract.contract import Contract
 import gym
 import copy 
 
+class GridGameContract(Contract):
+    def __init__(self, num_agents,low_val=0,high_val=2):
+        super().__init__(gym.spaces.Box(shape=(1,), low=low_val, high=high_val), np.array([0.0]), num_agents)
+
+    def compute_transfer(self, obs, acts, rews, params, infos=None):
+        """
+        obs: dict of observations
+        acts: dict of actions
+        rews: dict of rewards
+        params: dict of contract parameters
+        """
+        keys = list(acts.keys())
+        transfers = {}
+        for i in range(len(keys)):
+            transfers[keys[i]] = params[keys[i]][0] # transfer the contract value
+        return transfers
+
 class PDContract(Contract):
     """
     A contract space for a multi-agent prisoner dilemma environment.
